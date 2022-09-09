ef7bda8a81eb54c26e369539c876cec9b31f0468
jitendrakr54 <jitendrakr54@gmail.com>
2022-09-09 17:52:48 +0530

    Add chart feature

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
+import ExpensesChart from "./ExpensesChart";
+
 import "./Expenses.css";
 
 // function Expenses(props) {
@@ -23,6 +25,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
